d31e1f54eb324abb0ee7d26163a18fbbfe2bdb08
Astrid Nielsen Labs <32886080+AstridNielsen-lab@users.noreply.github.com>
2023-03-05 21:43:51 -0300

    Update search.tsx

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -29,9 +29,6 @@ if(e.key === "Enter"){
         onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)} ><FaSearch/></button>
-         <iframe src="https://discord.com/widget?id=1040174936544776202&theme=dark" width="350" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe> 
-      
-      
         
       </div>
     </div>
