b841f95a011dc29ef3f10763aa92a90f567a3e81
Astrid Nielsen Labs <32886080+AstridNielsen-lab@users.noreply.github.com>
2023-03-05 21:39:16 -0300

    Update search.tsx

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -29,12 +29,11 @@ if(e.key === "Enter"){
         onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)} ><FaSearch/></button>
+         <iframe src="https://discord.com/widget?id=1040174936544776202&theme=dark" width="350" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe> 
+      
+      
+        
       </div>
-      
-      <iframe src="https://discord.com/widget?id=1040174936544776202&theme=dark" width="350" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe> 
-      
-      
-      
     </div>
   )
 }
